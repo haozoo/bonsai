1dcf107dc9a037b32e2ef100edf947a73027d720
Hao Xu <b.hao.xu@gmail.com>
2021-04-28 14:58:35 +1000

    Can now change the symbol printed depending on L M R - also attempted to limit growth to tree's upper branches

--- bonsai.c
+++ bonsai.c
@@ -9,7 +9,7 @@
 #define SCR_WIDTH 100
 #define SCR_HEIGHT 50
 #define SCR_SIZE (SCR_WIDTH * SCR_HEIGHT)
-#define MAX_ITERS 100
+#define MAX_ITERS 500
 
 int main() {
   /* Generate seed using time */
@@ -21,6 +21,13 @@ int main() {
     pos[i] = 0;
   pos[0] = SCR_WIDTH / 2 + SCR_WIDTH * (SCR_HEIGHT / 2);
   int posLen = 1;
+
+  /* Array of potential lines */
+  char sym[SCR_SIZE];
+  for (int i = 0; i < SCR_SIZE; i++)
+    sym[i] = ' ';
+  sym[0] = '|';
+  int symLen = 1;
 
   /* Output */
   char output[SCR_SIZE];
@@ -36,7 +43,8 @@ int main() {
     int r = (rand() % (posLen + 1));
 
     /* 2. Use random number to pick from pos and use as index */
-    output[pos[r]] = '|';
+
+    output[pos[r]] = sym[r];
 
     /* 3. Delete selected random pos from array
      *  - Set pos[r]] to the last value in pos
@@ -45,16 +53,22 @@ int main() {
      */
     int tmp = pos[r];
     pos[r] = pos[posLen - 1];
+    sym[r] = sym[symLen - 1];
     pos[posLen - 1] = 0;
+    sym[symLen - 1] = ' ';
     posLen--;
+    symLen--;
 
+    /* 4. Add new pos to array */
     int L = 1, M = 1, R = 1;
     int LL = 1, RR = 1;
     for (int j = 0; j < posLen; j++) {
-      if (pos[j] == tmp - SCR_WIDTH - 1)
-        L = 0;
+
       if (pos[j] == tmp - SCR_WIDTH)
         M = 0;
+
+      if (pos[j] == tmp - SCR_WIDTH - 1)
+        L = 0;
       if (pos[j] == tmp - SCR_WIDTH + 1)
         R = 0;
 
@@ -64,22 +78,42 @@ int main() {
         RR = 0;
     }
 
+    if (M) {
+      pos[posLen++] = tmp;
+      sym[symLen++] = '|';
+    }
     if (L) {
       pos[posLen++] = tmp - SCR_WIDTH - 1;
-      pos[posLen++] = tmp - SCR_WIDTH - 1;
-      pos[posLen++] = tmp - SCR_WIDTH - 1;
+      sym[symLen++] = '_';
     }
-    if (M)
-      pos[posLen++] = tmp;
     if (R) {
       pos[posLen++] = tmp - SCR_WIDTH + 1;
-      pos[posLen++] = tmp - SCR_WIDTH + 1;
-      pos[posLen++] = tmp - SCR_WIDTH + 1;
+      sym[symLen++] = '_';
     }
-    if (LL)
+    if (LL) {
       pos[posLen++] = tmp - SCR_WIDTH - 2;
-    if (RR)
+      sym[symLen++] = '\\';
+    }
+    if (RR) {
       pos[posLen++] = tmp - SCR_WIDTH + 2;
+      sym[symLen++] = '/';
+    }
+
+    int max = 0;
+    for (int j = 0; j < posLen; j++) {
+      if (pos[j] > max)
+        max = pos[j];
+    }
+    for (int j = 0; j < posLen; j++) {
+      if (pos[j] / SCR_WIDTH < max / SCR_WIDTH) {
+        pos[j] = pos[posLen - 1];
+        sym[j] = sym[symLen - 1];
+        pos[posLen - 1] = 0;
+        sym[symLen - 1] = ' ';
+        posLen--;
+        symLen--;
+      }
+    }
 
     /* Print output[] to the screen*/
     printf("[H");
